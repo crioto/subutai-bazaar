2682efbbe5b7ec1cb60a833ab1f883f10d124e0c
Mike Savochkin <github@crioto.com>
2020-06-09 14:44:04 +0600

    Releasing 0.0.6
    Code cleanup and removing debug code

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version_number = "0.0.5"
+version_number = "0.0.6"
 
 setuptools.setup(
     name="subutai-bazaar",
